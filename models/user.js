2da8f5ae582da873c800f7c1f17c3eb15645bb3a
Roberto . <robertostephen2002@gmail.com>
2025-03-26 10:36:39 +0700

    Update Mantap

--- models/user.js
+++ models/user.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 const {Schema} = mongoose
 
-// Schema
 const userSchema = new Schema({
     username: {
         type: String,
@@ -11,6 +11,7 @@ const userSchema = new Schema({
         type: String,
         unique: true,
         required: [true, "Email is required"],
+        match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Email tidak valid']
     },
     number: {
         type: String,
@@ -19,30 +20,49 @@ const userSchema = new Schema({
     },
     address: {
         type: String,
-        unique: true,
         required: [true, "Address is required"],
     },
-    
     password: {
         type: String,
         required: [true, "Password is required"],
     },
     avatar_url: {
         type: String,
-        
     },
     type: {
         type: String,
+        enum: ['admin', 'client'], // Batasi tipe pengguna
         required: [true, "Type is required"],
     },
     rating: {
-        type: {
-
-        }
+        type: mongoose.Schema.Types.Mixed // Sesuaikan dengan kebutuhan rating
     }
-})
+}, { 
+    timestamps: true // Tambahkan createdAt dan updatedAt
+});
 
-// Model
+// Middleware untuk hash password sebelum disimpan
+userSchema.pre('save', async function(next) {
+    // Hanya hash password jika dimodifikasi
+    if (!this.isModified('password')) return next();
+
+    try {
+        // Generate salt
+        const salt = await bcrypt.genSalt(10);
+        
+        // Hash password
+        this.password = await bcrypt.hash(this.password, salt);
+        next();
+    } catch (error) {
+        next(error);
+    }
+});
+
+// Metode untuk validasi password
+userSchema.methods.validatePassword = async function(candidatePassword) {
+    return bcrypt.compare(candidatePassword, this.password);
+};
+
 const UserModel = mongoose.model('User', userSchema);
 
-module.exports = UserModel  
+module.exports = UserModel;
