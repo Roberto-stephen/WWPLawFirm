38d7226b8ddc9687a98d2b429b9af6561eb6ee11
Roberto . <robertostephen2002@gmail.com>
2025-03-27 11:54:32 +0700

    Tau lah

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -3,64 +3,62 @@ const { DoNotHaveAccessError } = require('../helpers/exceptions');
 const getUserInfo = require('../helpers/getUserInfo');
 
 const requireAuth = (req, res, next) => {
-    const { token } = req.cookies
-    if (token) {
-        jwt.verify(token, process.env.JWT_SECRET, {}, (err, decodedToken) => {
-            if (err) {
-                var code = 401
-                var errorInfo = "Unauthorized Access"
-                res.status(code).send({
-                    status: code,
-                    info: errorInfo,
-                    error_code: code,
-                    message: errorInfo,
-                });
-            }
-            else {
-                res.locals.decodedToken = decodedToken
-                next()
-            }
-        })
-    } else {
-        var code = 401
-        var errorInfo = "Unauthorized Access"
-        res.status(code).send({
-            status: code,
-            info: errorInfo,
-            error_code: code,
-            message: errorInfo,
+    // Cek token dari Authorization header atau cookies
+    const token = 
+        req.headers.authorization?.split(' ')[1] || // Bearer token
+        req.cookies?.token; // Cookie token
+
+    if (!token) {
+        return res.status(401).json({
+            status: 401,
+            info: "Unauthorized Access",
+            error_code: 401,
+            message: "No token provided"
+        });
+    }
+
+    try {
+        const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+        res.locals.decodedToken = decodedToken;
+        next();
+    } catch (err) {
+        return res.status(401).json({
+            status: 401,
+            info: "Unauthorized Access",
+            error_code: 401,
+            message: "Invalid or expired token"
         });
     }
 }
 
 const requireLawyerAndAdmin = (req, res, next) => {
-    const { type } = getUserInfo(res)
     try {
+        const { type } = getUserInfo(res);
         if (type === "client") {
-            throw new DoNotHaveAccessError("User do not have access to perform such action")
+            throw new DoNotHaveAccessError("User do not have access to perform such action");
         }
-        next()
+        next();
     } catch (error) {
-        res.status(400).json({
+        res.status(403).json({
             error: error.name,
             message: error.message
-        })
+        });
     }
 }
 
 const requireAdmin = (req, res, next) => {
-    const { type } = getUserInfo(res)
     try {
+        const { type } = getUserInfo(res);
         if (type !== "admin") {
-            throw new DoNotHaveAccessError("User do not have access to perform such action")
+            throw new DoNotHaveAccessError("User do not have access to perform such action");
         }
-        next()
+        next();
     } catch (error) {
-        res.status(400).json({
+        res.status(403).json({
             error: error.name,
             message: error.message
-        })
+        });
     }
 }
 
-module.exports = { requireAuth, requireLawyerAndAdmin, requireAdmin }
+module.exports = { requireAuth, requireLawyerAndAdmin, requireAdmin };
